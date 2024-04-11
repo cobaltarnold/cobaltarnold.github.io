fe6c88e8596c43205c779f4bb5cacc02918ef445
cobaltarnold <cobaltarnolds23@gmail.com>
2024-04-11 16:33:28 -0400

    touch fix?

--- js/apps/pixelfall.js
+++ js/apps/pixelfall.js
@@ -63,6 +63,19 @@ function setup() {
         clearInterval(counter);
     });
 
+    ["mousedown", "touchstart"].forEach(event => {
+        button.addEventListener(event, () => {
+            counter= setInterval(
+                process(pixarray)
+            , 30);
+        });
+    });
+    ["mouseup", "touchend"].forEach(event => {
+        button.addEventListener(event, () => {
+            clearInterval(counter);
+        });
+    });
+
     button2.mousePressed(() => {
         img.save('pixelfall');
     });
